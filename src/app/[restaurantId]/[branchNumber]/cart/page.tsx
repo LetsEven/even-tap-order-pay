6e281c8152d4c8d7ae0ac388c119d5c6e29e5cd9
leo-garcia-xquisito <leonardogarcia@letseven.io>
2026-05-29 09:40:14 -0600

    Set document title on menu and cart pages

--- src/app/[restaurantId]/[branchNumber]/cart/page.tsx
+++ src/app/[restaurantId]/[branchNumber]/cart/page.tsx
@@ -3,9 +3,17 @@
 import { useValidateAccess } from "@/hooks/useValidateAccess";
 import ValidationError from "@/components/ValidationError";
 import CartView from "@/components/CartView";
+import { useEffect } from "react";
 
 export default function CartPage() {
   const { validationError } = useValidateAccess();
+
+  useEffect(() => {
+    document.title = "Mi Carrito | Tap Order & Pay";
+    return () => {
+      document.title = "Even Tap Order & Pay";
+    };
+  }, []);
 
   // Mostrar error de validación
   if (validationError) {
